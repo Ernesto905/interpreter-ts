dada2f307801e65900d7ad095fbeb1fb64ca8c4a
ernesto905 <ernesto90643@gmail.com>
2025-11-14 14:38:29 -0500

    wrapped up test case

--- lexer/lexer.test.ts
+++ lexer/lexer.test.ts
@@ -1,12 +1,25 @@
 import { Lexer } from "./lexer";
+import * as token from "../token/token"
 
 test("Lexer tokenizes a simple string of tokens.", () => {
-  const lexer = new Lexer(input);
-
   const input = "=+(){},;";
 
+  const lexer = new Lexer(input);
+  const tests = {
+    "=": token.ASSIGN,
+    "(": token.LPAREN,
+    ")": token.RPAREN,
+    "{": token.LBRACE,
+    "}": token.RBRACE,
+    ",": token.COMMA,
+    ";": token.SEMICOLON,
+    "": token.EOF,
+  }
+
   for (let i: number = 0; i < input.length; i++) {
-    let { type, literal } = lexer.NextToken();
-    expect(types).toBe(input[i]);
+    let tok: token.Token = lexer.NextToken();
+
+    expect(tok.Type).toBe(tests[input[i]])
+    expect(tok.Literal).toBe(input[i])
   }
 })
--- lexer/lexer.ts
+++ lexer/lexer.ts
@@ -1,8 +1,10 @@
+import * as token from '../token/token';
+
 export class Lexer {
   input: string;
-  position: number; // ptr to input char that corresponds to ch
-  readPosition: number; // ptr to the next char in the input
-  ch: number; // current character  
+  position: number; // ptr to ch ; input[position] === ch
+  readPosition: number; // ptr to input[position] + 1
+  ch: number; // current character as a code point
 
   constructor(input: string) {
     this.input = input;
@@ -22,4 +24,47 @@ export class Lexer {
     this.position = this.readPosition;
   }
 
+  NextToken(): token.Token {
+    let tok: token.Token;
+
+    switch (this.ch) {
+      case "=".charCodeAt(0):
+        tok = this.newToken(token.ASSIGN, this.ch);
+        break;
+      case "(".charCodeAt(0):
+        tok = this.newToken(token.LPAREN, this.ch);
+        break;
+      case ")".charCodeAt(0):
+        tok = this.newToken(token.RPAREN, this.ch);
+        break;
+      case ",".charCodeAt(0):
+        tok = this.newToken(token.COMMA, this.ch);
+        break;
+      case "+".charCodeAt(0):
+        tok = this.newToken(token.PLUS, this.ch);
+        break;
+      case "{".charCodeAt(0):
+        tok = this.newToken(token.LBRACE, this.ch);
+        break;
+      case "}".charCodeAt(0):
+        tok = this.newToken(token.RBRACE, this.ch);
+        break;
+      case "0".charCodeAt(0):
+        tok = this.newToken(token.EOF, this.ch);
+        break;
+      default:
+        tok = this.newToken(token.ILLEGAL, this.ch);
+    }
+
+    this.readChar();
+    return tok
+  }
+
+  newToken(tokenType: token.TokenType, ch: number): token.Token {
+    const tok: token.Token = {
+      Type: tokenType,
+      Literal: String.fromCodePoint(ch)
+    };
+    return tok;
+  }
 }
